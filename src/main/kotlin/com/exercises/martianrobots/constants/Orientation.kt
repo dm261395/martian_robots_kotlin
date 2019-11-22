768b33542ac0b973e0b7cd3865a1b9b2db756715
Duncan MacKinnon <duncan.mackinnon@red-badger.com>
2019-11-22 12:09:53 +0000

    Orientation enum class methods to get new orientation for instruction

--- src/main/kotlin/com/exercises/martianrobots/constants/Orientation.kt
+++ src/main/kotlin/com/exercises/martianrobots/constants/Orientation.kt
@@ -1,5 +1,42 @@
 package com.exercises.martianrobots.constants
 
 enum class Orientation {
-    NORTH, EAST, SOUTH, WEST
+    NORTH {
+        override fun getNewOrientation(instruction: Instruction): Orientation {
+            return when (instruction) {
+                DirectionalInstruction.LEFT -> WEST
+                DirectionalInstruction.RIGHT -> EAST
+                else -> this
+            }
+        }
+    },
+    EAST {
+        override fun getNewOrientation(instruction: Instruction): Orientation {
+            return when (instruction) {
+                DirectionalInstruction.LEFT -> NORTH
+                DirectionalInstruction.RIGHT -> SOUTH
+                else -> this
+            }
+        }
+    },
+    SOUTH {
+        override fun getNewOrientation(instruction: Instruction): Orientation {
+            return when (instruction) {
+                DirectionalInstruction.LEFT -> EAST
+                DirectionalInstruction.RIGHT -> WEST
+                else -> this
+            }
+        }
+    },
+    WEST {
+        override fun getNewOrientation(instruction: Instruction): Orientation {
+            return when (instruction) {
+                DirectionalInstruction.LEFT -> SOUTH
+                DirectionalInstruction.RIGHT -> NORTH
+                else -> this
+            }
+        }
+    };
+
+    abstract fun getNewOrientation(instruction: Instruction): Orientation
 }
