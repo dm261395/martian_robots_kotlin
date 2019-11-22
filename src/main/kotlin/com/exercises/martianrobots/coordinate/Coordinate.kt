50b522760e3b971b15a67e72700411a21937ad38
Duncan MacKinnon <duncan.mackinnon@red-badger.com>
2019-11-22 12:10:16 +0000

    Coordinate toString method

--- src/main/kotlin/com/exercises/martianrobots/coordinate/Coordinate.kt
+++ src/main/kotlin/com/exercises/martianrobots/coordinate/Coordinate.kt
@@ -2,5 +2,7 @@ package com.exercises.martianrobots.coordinate
 
 data class Coordinate(val x: Int, val y: Int) {
     operator fun compareTo(other: Coordinate): Int = (other.x + other.y) - (x + y)
+
+    override fun toString(): String = "$x $y"
 }
 
