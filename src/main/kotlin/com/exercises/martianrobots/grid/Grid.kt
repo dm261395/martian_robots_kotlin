302cd37d5bddba2901b53ecbf976f61ced949619
Duncan MacKinnon <duncan.mackinnon@red-badger.com>
2019-11-25 17:19:30 +0000

    Using ==

--- src/main/kotlin/com/exercises/martianrobots/grid/Grid.kt
+++ src/main/kotlin/com/exercises/martianrobots/grid/Grid.kt
@@ -6,7 +6,7 @@ class Grid(private val topRightCoords: Coordinate) {
     private var scentsCoords: MutableList<Coordinate> = mutableListOf()
     private val bottomLeftCoords: Coordinate = Coordinate(0, 0)
 
-    fun hasScentAtCoordinates(coords: Coordinate): Boolean = scentsCoords.find { it.equals(coords) } != null
+    fun hasScentAtCoordinates(coords: Coordinate): Boolean = scentsCoords.find { it == coords } != null
 
     fun coordinatesAreInBounds(coords: Coordinate): Boolean =
         bottomLeftCoords.x <= coords.x && coords.x <= topRightCoords.x && bottomLeftCoords.y <= coords.y && coords.y <= topRightCoords.y
